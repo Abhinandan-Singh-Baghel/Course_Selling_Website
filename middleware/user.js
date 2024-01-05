a1fa6a212dc4c1d1ce33ba954fcabd3be0d8f3bd
Abhinandan Singh Baghel <abhinandansinghbaghel2001@gmail.com>
2024-01-05 23:13:57 +0530

    Readme

---
+++ middleware/user.js
@@ -0,0 +1,6 @@
+const { User } = require("../db");
+
+function userMiddleware(req, res, next){
+    // Implement admin auth logic
+    // You need to check the headers and validate the admin from the admin DB.
+}
